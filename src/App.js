e8843a1f9a45ef61947f501880379d9febafc9c9
ivangeorgiev97 <georgiev1313@abv.bg>
2024-09-01 18:15:18 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useEffect, useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [secondCounter, setSecondCounter] = useState(0);
+
+  const handleIncreaseClick = () => {
+    setCounter((counter) => counter + 1);
+  };
+
+  const handleSecondIncreaseClick = useCallback(() => {
+    setSecondCounter((secondCounter) => secondCounter + 1);
+  }, []);
+
+  useEffect(() => {
+    alert("I run only on initial component rendering");
+  }, []);
+
+  useEffect(() => {
+    alert("I run on each component re-render");
+  });
+
+  useEffect(() => {
+    counter && alert("I run when counter value changes and is above 0");
+  }, [counter]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>First counter: {counter}</p>
+
+      <button onClick={handleIncreaseClick}>Increase</button>
+
+      <p>Second counter: {secondCounter}</p>
+
+      <button onClick={handleSecondIncreaseClick}>Increase second</button>
     </div>
   );
 }
